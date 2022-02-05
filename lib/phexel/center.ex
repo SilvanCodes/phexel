c45db1ffabd041a6f7de43561a4b76faeddabe2e
Silvan Buedenbender <silvancodes@gmail.com>
2022-02-05 15:46:22 +0100

    Adapt center to 3rd edition of every layout

--- lib/phexel/center.ex
+++ lib/phexel/center.ex
@@ -1,9 +1,17 @@
 defmodule Phexel.Center do
   use Phoenix.Component
 
+  import Phexel, only: [put_configuration: 2]
+
+  @configuration [:"center-padding-inline"]
+
   def center(assigns) do
+    assigns =
+      assigns
+      |> put_configuration(@configuration)
+
     ~H"""
-      <div class="elc-center">
+      <div class="elc-center" {@configuration}>
         <%= render_slot(@inner_block) %>
       </div>
     """
